a47ba1f7b59ad146c19d6775ec37058b0aa39e99
astaria <astaria@gmx.de>
2017-03-11 10:07:48 +0100

    initial checkin

---
+++ ExpandRegions/ExpandRegions/RegionTextViewHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
+
+namespace ExpandRegions
+{
+    class RegionTextViewHandler
+    {
+        private IWpfTextView _textView;
+        private IOutliningManager _outliningManager;
+
+        public static void CreateHandler(IWpfTextView textView, IOutliningManagerService outliningManagerService)
+        {
+            new RegionTextViewHandler(textView, outliningManagerService);
+        }
+
+        private RegionTextViewHandler(IWpfTextView textView, IOutliningManagerService outliningManagerService)
+        {
+            
+            _outliningManager = outliningManagerService.GetOutliningManager(textView);
+            if (_outliningManager == null)
+            {
+                return;
+            }
+            _textView = textView;
+            _outliningManager.RegionsCollapsed += OnRegionsCollapsed;
+            _textView.Closed += OnClosed;
+
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            if (_outliningManager != null)
+            {
+                _outliningManager.RegionsCollapsed -= OnRegionsCollapsed;
+            }
+            if (_textView != null)
+            {
+                _textView.Closed -= OnClosed;
+            }
+            _textView = null;
+            _outliningManager = null;
+        }
+        
+        private void OnRegionsCollapsed(object sender, RegionsCollapsedEventArgs e)
+        {
+            foreach (var cr in e.CollapsedRegions)
+            {
+                _outliningManager.Expand(cr);
+            }
+            _outliningManager.RegionsCollapsed -= OnRegionsCollapsed;
+        }
+
+       
+    }
+}
---
+++ ExpandRegions/ExpandRegions/TextViewCreationListener.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
+using Microsoft.VisualStudio.Utilities;
+
+namespace ExpandRegions
+{
+    [TextViewRole("DOCUMENT"), ContentType("CSharp"), ContentType("Basic"), Export(typeof(IWpfTextViewCreationListener))]
+    public class TextViewCreationListener : IWpfTextViewCreationListener
+    {
+      
+
+        [Import(typeof(IOutliningManagerService))]
+        public IOutliningManagerService OutliningManagerService
+        {
+            get;
+            set;
+        }
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            if (textView == null || OutliningManagerService == null)
+            {
+                return;
+            }
+            RegionTextViewHandler.CreateHandler(textView, OutliningManagerService);
+        }
+    }
+}
